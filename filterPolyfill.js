b9c1039faabee24e5a67e99fc3a9392b2615ca68
VijayGit-Hub <dwivedivijayprakash@yahoo.in>
2025-10-18 16:47:00 +0530

    add my own

--- filterPolyfill.js
+++ filterPolyfill.js
@@ -1,13 +1,40 @@
 // The filter method of javascript
 
-let arr3 = [1,2,3,4,5,6]
+// let arr3 = [1,2,3,4,5,6]
 
-let filter = arr3.filter( (item , index , arr) => {
-    console.log("item" , item);
-    console.log("index" , index);
-    console.log("arr" , arr);
+// let filter = arr3.filter( (item , index , arr) => {
+//     console.log("item" , item);
+//     console.log("index" , index);
+//     console.log("arr" , arr);
 
-    return item > 3
-} )
+//     return item > 3
+// } )
 
-console.log("filter" , filter);
+// console.log("filter" , filter);
+
+// Polyfill of filter method
+
+Array.prototype.myfilter = function(cb) {
+
+    let filteredArray = []
+
+    for(let i = 0 ; i < this.length ; i++){
+
+        if(cb(this[i] , 1 , this)) { 
+         filteredArray.push(this[i])
+        }
+    }
+
+    return filteredArray
+}
+
+// Testing my own polyfill now 
+
+let arr  = [1,2,3,4,5]
+
+let result  = arr.myfilter( (value ,  index , arr) => {
+
+    return value % 2 === 0 
+})
+
+console.log(result);
