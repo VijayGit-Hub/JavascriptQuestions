5ae6bde204826987e98d8e327fc39e27744c2233
VijayGit-Hub <dwivedivijayprakash@yahoo.in>
2025-10-18 17:25:59 +0530

    reduce polyfill implementation

--- reducePolyfill.js
+++ reducePolyfill.js
@@ -1,10 +1,41 @@
 // The reduce method of javescript 
 
-let arr = [1,2,3,4,5,6]
+// let arr = [1,2,3,4,5,6]
 
-let sum = arr.reduce( (prev , current , currIndex , arr) => {
+// let sum = arr.reduce( (prev , current , currIndex , arr) => {
 
-    return prev + current
-} )
+//     return prev + current
+// } , 0 )
 
-console.log("sum" , sum);
+// console.log("sum" , sum);
+
+// Polyfill of reduce method 
+
+
+Array.prototype.myreduce = function(cb , initalvalue) {
+
+    let result ; 
+
+    let internalInitialValue = initalvalue ;
+
+    for(let i = 0 ; i < this.length ; i++) {
+        
+       result  =  cb(internalInitialValue , this[i] , i , this)
+       internalInitialValue = result
+   
+
+
+    }
+    return result
+}
+
+// Testing my reduce locally on my own array 
+
+let testarray = [2,5,6,7,3,4]
+
+let result = testarray.myreduce( (prev , current , index , array) => {
+
+    return prev + current 
+} , 3)
+
+console.log("result" , result); // result 30
